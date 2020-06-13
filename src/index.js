478f696f912b443e65e1dd14c1f603a92045ed96
Рябов <dmitryabov2017@yandex.ru>
2020-06-13 12:35:14 +0300

    7. Личный проект: валидируй, как никогда не валидировал

--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ const Movie = {
   year: `2014`
 };
 
+const movieNames = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `The Man Called Flintstone`,
+  `Casper the Friendly Ghost`, `Peppa Pig`, `Aladdin`, `Finding Nemo`, `Toy Story`];
+
 ReactDOM.render(
     <App
-      movieName={Movie.movieName} genre={Movie.genre} year={Movie.year}/>,
+      movieName={Movie.movieName} genre={Movie.genre} year={Movie.year} movieNames={movieNames}/>,
     document.querySelector(`#root`)
 );
